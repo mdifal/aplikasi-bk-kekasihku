e34603e94cfb3c4031647ef1cce73a6d594cebc6
nisrinawafaz <nisrinawafaz@gmail.com>
2025-01-02 00:54:39 +0700

    update layout

--- FE/src/layouts/index.jsx
+++ FE/src/layouts/index.jsx
@@ -1,26 +1,20 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 import Coba from "../pages/murid";
+import BimbinganBelajar from "../pages/guru/data/bimbingan_belajar/index";
 import Typography from "@mui/material/Typography";
 import Login from "../pages/guru/login";
 import Register from "../pages/guru/register";
 // import PrivateRoute from "../services/PrivateRoute";
 
-// import Navbar from "../components/Navbar";
-
 function Layout() {
   const location = useLocation();
-  const path = location.pathname;
-
-  const isLogin = path === "/";
-  const isRegister = path === "/signup";
 
   return (
     <>
-    
-      {/* {isLogin || isRegister ? <></> : <Navbar />} */}
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Register />} />
+        <Route path="/bimbingan-belajar" element={<BimbinganBelajar />} />
         <Route path="/coba" element={<Coba />} />
         {/* <Route path="/*" element={<PrivateRoute />}>*/}
       </Routes>
